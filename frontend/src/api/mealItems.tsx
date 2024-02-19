9ea774c6e83635ba815a570e7e66811a804ea9ab
Saint1225 <vincent-valentine1225@hotmail.com>
2024-02-19 23:59:28 +0800

    Add Link and Cart button in Nav

--- frontend/src/api/mealItems.tsx
+++ frontend/src/api/mealItems.tsx
@@ -5,7 +5,6 @@ export const fetchMealItems = (): Promise<mealItemType[]> => {
   const meals = axios
     .get("http://localhost:8010/proxy/meals/mealItems")
     .then((res) => {
-      console.log(res.data);
       return res.data;
     })
     .catch((error) => console.log(error));
--- frontend/src/routes/MainNavigation.tsx
+++ frontend/src/routes/MainNavigation.tsx
@@ -1,7 +1,29 @@
+import { NavLink } from "react-router-dom";
+
 const MainNavigation = () => {
   return (
-    <div className="h-12 w-full bg-slate-400 text-blueGray-900 text-center">
-      Navigation
+    <div className="h-12 w-full text-yellow-400 bg-blueGray-900 text-center">
+      <div className="flex flex-row justify-center gap-36 text-4xl h-full">
+        <NavLink
+          to="/"
+          className="hover:bg-blueGray-800 rounded-lg px-3 my-1 ml-[35%]"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="meals"
+          className="hover:bg-blueGray-800 rounded-lg px-3 my-1"
+        >
+          Menu
+        </NavLink>
+        <button className="ml-auto mr-4">Cart</button>
+      </div>
+      <dialog title="Cart" className="w-96 h-48">
+        <form method="dialog">
+          <button className="">Cancel</button>
+          <button className="">Submit</button>
+        </form>
+      </dialog>
     </div>
   );
 };
