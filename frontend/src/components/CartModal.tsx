984b63a48e348a051b67e72d17bce1ea0ca1c82f
Saint1225 <vincent-valentine1225@hotmail.com>
2024-02-24 23:59:49 +0800

    Add cart modal

---
+++ frontend/src/components/CartModal.tsx
@@ -0,0 +1,32 @@
+import { forwardRef, useRef, useImperativeHandle } from "react";
+
+const CartModal = forwardRef((props, ref) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      openModal: () => dialogRef.current && dialogRef.current.showModal(),
+    };
+  });
+
+  return (
+    <dialog
+      ref={dialogRef}
+      title="Cart"
+      className="w-2/5 h-2/5 bg-blueGray-800 text-zinc-300 rounded-xl border-solid border-4 border-orange-400"
+    >
+      <form method="dialog">
+        <h2 className="text-3xl my-6 font-bold">Orders</h2>
+        <div className="flex flex-row gap-40 justify-center text-2xl mx-3">
+          <button className="border-solid border-2 text-orange-400 border-orange-400 rounded-md px-2 py-1 hover:border-orange-200 hover:text-orange-300">
+            Cancel
+          </button>
+          <button className="border-solid border-2 text-orange-400 border-orange-400 rounded-md px-2 py-1 hover:border-orange-200 hover:text-orange-300">
+            Submit
+          </button>
+        </div>
+      </form>
+    </dialog>
+  );
+});
+export default CartModal;
--- frontend/src/routes/MainNavigation.tsx
+++ frontend/src/routes/MainNavigation.tsx
@@ -1,6 +1,14 @@
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
+import CartModal from "../components/CartModal";
 
 const MainNavigation = () => {
+  const cartRef = useRef<{ openModal: () => void }>(null);
+
+  const cartModalHandler = () => {
+    cartRef.current?.openModal();
+  };
+
   return (
     <div className="h-12 w-full text-yellow-400 bg-blueGray-900 text-center">
       <div className="flex flex-row justify-center gap-36 text-4xl h-full overflow-hidden">
@@ -17,16 +25,14 @@ const MainNavigation = () => {
         >
           Menu
         </NavLink>
-        <button className="ml-auto mr-8 rounded-xl text-3xl border-solid border-orange-400 border-2 px-4 hover:bg-blueGray-800">
+        <button
+          className="ml-auto mr-8 rounded-xl text-3xl border-solid border-orange-400 border-2 px-4 hover:bg-blueGray-800"
+          onClick={cartModalHandler}
+        >
           Orders
         </button>
       </div>
-      <dialog title="Cart" className="w-96 h-48">
-        <form method="dialog">
-          <button className="">Cancel</button>
-          <button className="">Submit</button>
-        </form>
-      </dialog>
+      <CartModal ref={cartRef} />
     </div>
   );
 };
